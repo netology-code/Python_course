a1ab54e576136863d20f94dfa355aa1b4daf2404
Oleg Blokhin <blohin.o.d@gmail.com>
2016-11-21 17:42:49 +0300

    Добавление простейшей программы с комментариями

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+# Ctrl+R cmd.exe
+# terminal.app
+
+# чтобы запустить скрипт
+# надо перейти в папку с этим скриптом
+# cd Documents/Netology_Python_course/shared_materials/
+
+# запускать скрипт
+# python helloworld.py
+# либо
+# python3 helloworld.py
+# как првоерить версию?
+# python --version
+
+print('helloworld')
